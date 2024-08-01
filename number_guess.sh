8ce6371afa952a501d057d836f28753888c1e11d
Thomas Cansino <thomas.cansino@gmail.com>
2024-08-01 15:07:51 +0000

    feat: added sql database & edited number guess script

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # A script that generates a random number that users have to guess
+
+echo "Enter your username:"
+read USERNAME
